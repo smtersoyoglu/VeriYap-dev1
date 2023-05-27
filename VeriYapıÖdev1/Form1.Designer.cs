00fef197fce5c53b81415cbde0a7c9b6431c8902
smter <smter@DESKTOP-96JC8IR>
2023-05-27 16:24:03 +0300

    Add project files.

---
+++ VeriYapıÖdev1/Form1.Designer.cs
@@ -0,0 +1,233 @@
+﻿namespace VeriYapıÖdev1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtNo = new System.Windows.Forms.TextBox();
+            this.txtSoyad = new System.Windows.Forms.TextBox();
+            this.txtAd = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnSonEkle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnListe = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.btnAraEkle = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtNo);
+            this.groupBox1.Controls.Add(this.txtSoyad);
+            this.groupBox1.Controls.Add(this.txtAd);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(121, 34);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(275, 176);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "TekBagliListe";
+            // 
+            // txtNo
+            // 
+            this.txtNo.Location = new System.Drawing.Point(127, 122);
+            this.txtNo.Name = "txtNo";
+            this.txtNo.Size = new System.Drawing.Size(117, 20);
+            this.txtNo.TabIndex = 5;
+            this.txtNo.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtNo_KeyPress);
+            // 
+            // txtSoyad
+            // 
+            this.txtSoyad.Location = new System.Drawing.Point(127, 80);
+            this.txtSoyad.Name = "txtSoyad";
+            this.txtSoyad.Size = new System.Drawing.Size(117, 20);
+            this.txtSoyad.TabIndex = 4;
+            this.txtSoyad.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSoyad_KeyPress);
+            // 
+            // txtAd
+            // 
+            this.txtAd.Location = new System.Drawing.Point(127, 37);
+            this.txtAd.Name = "txtAd";
+            this.txtAd.Size = new System.Drawing.Size(117, 20);
+            this.txtAd.TabIndex = 3;
+            this.txtAd.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAd_KeyPress);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(31, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 19);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Numara :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(31, 82);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Soyad : ";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(31, 38);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 19);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Ad : ";
+            // 
+            // btnSonEkle
+            // 
+            this.btnSonEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSonEkle.Location = new System.Drawing.Point(449, 52);
+            this.btnSonEkle.Name = "btnSonEkle";
+            this.btnSonEkle.Size = new System.Drawing.Size(93, 36);
+            this.btnSonEkle.TabIndex = 3;
+            this.btnSonEkle.Text = "Sona Ekle";
+            this.btnSonEkle.UseVisualStyleBackColor = true;
+            this.btnSonEkle.Click += new System.EventHandler(this.btnSonEkle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnSil.Location = new System.Drawing.Point(449, 155);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(93, 36);
+            this.btnSil.TabIndex = 4;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnListe
+            // 
+            this.btnListe.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnListe.Location = new System.Drawing.Point(248, 230);
+            this.btnListe.Name = "btnListe";
+            this.btnListe.Size = new System.Drawing.Size(117, 36);
+            this.btnListe.TabIndex = 5;
+            this.btnListe.Text = "Listeyi Yaz";
+            this.btnListe.UseVisualStyleBackColor = true;
+            this.btnListe.Click += new System.EventHandler(this.btnListe_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3});
+            this.dataGridView1.Location = new System.Drawing.Point(90, 282);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(435, 149);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "Ad";
+            this.Column1.Name = "Column1";
+            this.Column1.Width = 130;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "Soyad";
+            this.Column2.Name = "Column2";
+            this.Column2.Width = 130;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "Numara";
+            this.Column3.Name = "Column3";
+            this.Column3.Width = 130;
+            // 
+            // btnAraEkle
+            // 
+            this.btnAraEkle.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnAraEkle.Location = new System.Drawing.Point(449, 104);
+            this.btnAraEkle.Name = "btnAraEkle";
+            this.btnAraEkle.Size = new System.Drawing.Size(93, 36);
+            this.btnAraEkle.TabIndex = 2;
+            this.btnAraEkle.Text = "Araya Ekle";
+            this.btnAraEkle.UseVisualStyleBackColor = true;
+            this.btnAraEkle.Click += new System.EventHandler(this.btnAraEkle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.SteelBlue;
+            this.ClientSize = new System.Drawing.Size(621, 455);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnListe);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnSonEkle);
+            this.Controls.Add(this.btnAraEkle);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtNo;
+        private System.Windows.Forms.TextBox txtSoyad;
+        private System.Windows.Forms.TextBox txtAd;
+        private System.Windows.Forms.Button btnSonEkle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnListe;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.Button btnAraEkle;
+    }
+}
+
---
+++ VeriYapıÖdev1/Form1.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace VeriYapıÖdev1
+{
+    // Samet Ersoyoğlu - 213908354
+
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public class tekDugum
+        {
+            public String ad;
+            public String soyad;
+            public int no;
+            public tekDugum next;
+        }
+        tekDugum ilk = null;
+        tekDugum son = null;
+
+        private void btnSonEkle_Click(object sender, EventArgs e)
+        {
+            tekDugum tekdugum = new tekDugum();
+
+            if (txtAd.Text == "" || txtSoyad.Text == "" || txtNo.Text == "")
+            {
+                MessageBox.Show("Lütfen Eklemek İstediğiniz Kişinin Bilgilerini Giriniz, Boş Geçilemez !");
+
+            }
+            else
+            {
+                tekdugum.ad = txtAd.Text;
+                tekdugum.soyad = txtSoyad.Text;
+                tekdugum.no = Convert.ToInt32(txtNo.Text);
+                txtAd.Clear();
+                txtSoyad.Clear();
+                txtNo.Clear();
+
+                if (ilk == null)
+                {
+                    ilk = tekdugum;
+                    son = ilk;
+                    son.next = null;
+                }
+                else
+                {
+                    son.next = tekdugum;
+                    tekdugum.next = null;
+                    son = tekdugum;
+                }
+            }
+        }
+
+        private void btnAraEkle_Click(object sender, EventArgs e)
+        {
+            tekDugum tekdugum = new tekDugum();
+            tekDugum gecici = new tekDugum();
+
+
+            if (txtAd.Text == "" || txtSoyad.Text == "" || txtNo.Text == "")
+            {
+                MessageBox.Show("Lütfen Araya Eklemek İstediğiniz Kişinin Bilgilerini Giriniz, Boş Geçilemez !");
+
+            }
+            else
+            {
+                tekdugum.ad = txtAd.Text;
+                tekdugum.soyad = txtSoyad.Text;
+                tekdugum.no = Convert.ToInt32(txtNo.Text);
+                gecici = ilk;
+
+                if (ilk != null)
+                {
+                    while (gecici.no < tekdugum.no)
+                    {
+                        if (gecici.next.no > tekdugum.no)
+                        {
+                            break;
+                        }
+                        gecici = gecici.next;
+                    }
+                    tekdugum.next = gecici.next;
+                    gecici.next = tekdugum;
+                }
+            }
+        }
+
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            //noSil silinecek numara
+            //gecSil silinecek numaranın bir önceki numara
+            tekDugum noSil = new tekDugum();
+            tekDugum gecsil = new tekDugum();
+
+            if (txtNo.Text == "")
+            {
+                MessageBox.Show("Lütfen Silmek İstediğiniz Kişinin Numarasını Giriniz. Boş Geçilemez");
+
+            }
+            else
+            {
+                int no = Convert.ToInt32(txtNo.Text);
+                noSil = ilk;
+                txtNo.Clear();
+
+                if (ilk.no == no)
+                {
+                    // ilk numara silinme
+                    ilk = ilk.next;
+                }
+
+                else
+                {
+                    while (noSil.no != no)
+                    {
+                        gecsil = noSil;
+                        noSil= noSil.next;
+                    }
+
+                    gecsil.next = noSil.next;
+
+                }
+
+            }
+
+        }
+
+        public void listeYaz()
+        {
+            dataGridView1.Rows.Clear();
+            tekDugum bulSil = ilk;
+            while (bulSil != null)
+            {
+                dataGridView1.Rows.Add(bulSil.ad, bulSil.soyad, bulSil.no);
+                bulSil = bulSil.next;
+            }
+
+        }
+
+        private void btnListe_Click(object sender, EventArgs e)
+        {
+            listeYaz();
+        }
+
+        private void txtNo_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar);
+        }
+
+        private void txtAd_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+                && !char.IsSeparator(e.KeyChar);
+        }
+
+        private void txtSoyad_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar)
+               && !char.IsSeparator(e.KeyChar);
+        }
+    }
+}
